731fb8f61178bcdd4f85d6109258f011d9d83155
Mike <mike.kangaroo.world@gmail.com>
2023-10-21 14:42:41 +0900

    [feat] cleanup and refactoring

--- shop-moll/src/main.js
+++ shop-moll/src/main.js
@@ -1,27 +1,5 @@
 import dummy from "../dummy.json?raw";
 
-function getProductHTML(product, count = 0) {
-  return `
-    <div
-      class="product" 
-      data-product-id="${product.id}" 
-    >
-    <img src="${product.images[0]}" alt="Image of ${product.name}" /> 
-    <p>${product.name}</p>
-    <div class="flex items-center justify-between">
-      <span>Price: ${product.regularPrice}</span>
-      <div>
-        <button type="button" class="btn-increase bg-green-200 hover:bg-green-400 py-1 px-3 rounded-full ">+</button>
-        <span class="cart-count text-green-800">
-          ${count}
-        </span>
-        <button type="button" class="btn-decrease bg-green-200 hover:bg-green-400 py-1 px-3 rounded-full ">-</button>
-      </div>
-    </div>
-  </div>
-  `;
-}
-
 async function getProducts() {
   if (process.env.NODE_ENV === "development") {
     return JSON.parse(dummy);
@@ -51,13 +29,82 @@ function sumAllCounts(countMap) {
   }, 0);
 }
 
+function getProductHTML(product, count = 0) {
+  return `
+    <div
+      class="product" 
+      data-product-id="${product.id}" 
+    >
+    <img src="${product.images[0]}" alt="Image of ${product.name}" /> 
+    <p>${product.name}</p>
+    <div class="flex items-center justify-between">
+      <span>Price: ${product.regularPrice}</span>
+      <div>
+        <button type="button" class="btn-decrease disabled:cursor-not-allowed disabled:opacity-50 bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">-</button>
+        <span class="cart-count text-green-800">
+          ${count}
+        </span>
+        <button type="button" class="btn-increase bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">+</button>
+      </div>
+    </div>
+  </div>
+  `;
+}
+
 async function main() {
   const products = await getProducts();
-  const countMap = {};
   const productMap = {};
   products.forEach((product) => {
     productMap[product.id] = product;
   });
+
+  const countMap = {};
+
+  const updateProductCount = (productId) => {
+    const productElement = document.querySelector(
+      `.product[data-product-id='${productId}']`
+    );
+    const cartCountElement = productElement.querySelector(".cart-count");
+    cartCountElement.innerHTML = countMap[productId];
+    if (countMap[productId] === 0) {
+      cartCountElement.innerHTML = "0";
+    }
+  };
+
+  const updateCart = () => {
+    const productIds = Object.keys(countMap);
+    document.querySelector(".cart-items").innerHTML = productIds
+      .map((productId) => {
+        const productInCart = productMap[productId];
+        if (countMap[productId] === 0) {
+          return "";
+        }
+        return getProductHTML(productInCart, countMap[productId]);
+      })
+      .join("");
+
+    document.querySelector(".total-count").innerHTML = `${sumAllCounts(
+      countMap
+    )}`;
+  };
+
+  const increaseCount = (productId) => {
+    if (countMap[productId] === undefined) {
+      countMap[productId] = 0;
+    }
+    countMap[productId] += 1;
+    updateProductCount(productId);
+    updateCart();
+  };
+  const decreaseCount = (productId) => {
+    if (countMap[productId] === undefined) {
+      countMap[productId] = 0;
+    } else if (countMap[productId] > 0) {
+      countMap[productId] -= 1;
+      updateProductCount(productId);
+      updateCart();
+    }
+  };
 
   document.querySelector("#products").innerHTML = products
     .map((product) => getProductHTML(product))
@@ -70,41 +117,41 @@ async function main() {
     const productElement = findElement(targetElement, ".product");
     const productId = productElement.getAttribute("data-product-id");
     // const product = products.find((product) => product.id === productId);
-    const product = productMap[productId];
 
     if (
       targetElement.matches(".btn-decrease") ||
       targetElement.matches(".btn-increase")
     ) {
-      if (countMap[productId] === undefined) {
-        countMap[productId] = 0;
-      }
+      // if (countMap[productId] === undefined) {
+      //   countMap[productId] = 0;
+      // }
       if (targetElement.matches(".btn-decrease")) {
-        if (countMap[productId] > 0) {
-          countMap[productId] -= 1;
-        }
+        decreaseCount(productId);
       } else if (targetElement.matches(".btn-increase")) {
-        countMap[productId] += 1;
-      }
-
-      const cartCount = productElement.querySelector(".cart-count");
-      cartCount.innerHTML = countMap[productId];
-      if (countMap[productId] === 0) {
-        cartCount.innerHTML = 0;
-      } else {
-        const productIds = Object.keys(countMap);
-        console.log("productIds", productIds);
-        document.querySelector(".cart-items").innerHTML = productIds
-          .map((productId) => {
-            const productInCart = productMap[productId];
-            return getProductHTML(productInCart, countMap[productId]);
-          })
-          .join("");
+        increaseCount(productId);
       }
     }
+  });
 
-    const totalCount = sumAllCounts(countMap);
-    document.querySelector(".total-count").innerHTML = `(${totalCount})`;
+  document.querySelector(".cart-items").addEventListener("click", () => {
+    const targetElement = event.target;
+    const productElement = findElement(targetElement, ".product");
+    const productId = productElement.getAttribute("data-product-id");
+    // const product = products.find((product) => product.id === productId);
+
+    if (
+      targetElement.matches(".btn-decrease") ||
+      targetElement.matches(".btn-increase")
+    ) {
+      // if (countMap[productId] === undefined) {
+      //   countMap[productId] = 0;
+      // }
+      if (targetElement.matches(".btn-decrease")) {
+        decreaseCount(productId);
+      } else if (targetElement.matches(".btn-increase")) {
+        increaseCount(productId);
+      }
+    }
   });
 
   document.querySelector(".btn-cart").addEventListener("click", () => {
