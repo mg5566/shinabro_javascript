f3a9eff2c3012e6264c9ae0b8aa07cfceb4cd3c6
Mike <mike.kangaroo.world@gmail.com>
2023-12-11 19:59:29 +0900

    [feat] create miniquery class

--- mini-query/packages/library/src/main.ts
+++ mini-query/packages/library/src/main.ts
@@ -1,1 +1,14 @@
-export const name = "Mike";
+class MiniQuery {
+  private elements: Element[];
+  constructor(selector: string) {
+    this.elements = Array.from(document.querySelectorAll(selector));
+  }
+
+  length() {
+    return this.elements.length;
+  }
+}
+
+export const $ = (selector: string) => {
+  return new MiniQuery(selector);
+};
